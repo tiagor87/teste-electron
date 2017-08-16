1577df7ad54f1a5cb874a279486abd27d832c814
Tiago de C. Resende <tiagor87@MacBook-Pro-de-Tiago-2.local>
2017-08-15 23:53:57 -0300

    Remove *.js

--- src/electron/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var path = require("path");
-exports.server = express();
-exports.server.get('/', express.static(path.join(__dirname, 'dist')));
-exports.server.get('/api/hello-world', function (request, response) {
-    return response.status(200).json({ message: 'Hello World' });
-});
